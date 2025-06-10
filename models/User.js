8188e9b9da8fe23a26009b6aaa2af76a28806b36
rishavadhikari4 <rishavadhikari657@gmail.com>
2025-06-10 21:38:15 +0545

    added the changes for the profile

--- models/User.js
+++ models/User.js
@@ -25,7 +25,17 @@ const userSchema = new mongoose.Schema({
         type:Date,
         default:null
     }
-}, { timestamps: true });
+}, { 
+    timestamps: true 
+});
+
+// Middleware to handle profieImage default logic
+userSchema.pre('save', function(next) {
+    if (!this.profieImage || this.profieImage.trim() === '') {
+        this.profieImage = "https://res.cloudinary.com/de1lvlqme/image/upload/v1749566197/vecteezy_default-profile-account-unknown-icon-black-silhouette_20765399_ldtak0.jpg";
+    }
+    next();
+});
 
 const User = mongoose.model('User', userSchema);
 
